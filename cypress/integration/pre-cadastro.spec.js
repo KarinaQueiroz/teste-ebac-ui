1548834c34afb066cb0b9cc9e01cf26e923d0a92
KarinaQueiroz <kamqueiroz@gmail.com>
2022-04-20 17:09:31 -0300

    Usando custom commands

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -24,8 +24,11 @@ describe('Funcionalidade Pré Cadastro', () => {
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
 
+    });
 
-         
-
+    it.only('Deve completar o pré cadastro com sucesso usando Comandos customizados', () => {
+        let emailFaker2 = faker.internet.email()
+        cy.preCadastro(emailFaker2, 'senha!@#forte', 'Karina', 'Queiroz')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
 });
--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -16,7 +16,7 @@ describe('Funcionalidade Página de produtos', () => {
             .click()
     });
 
-    it.only('Deve adicionar um produto ao carrinho', () => {
+    it('Deve adicionar um produto ao carrinho', () => {
         var quantidade = 3
         cy.get('.product-block')
         .contains('Aero Daily Fitness Tee').click()
@@ -29,6 +29,14 @@ describe('Funcionalidade Página de produtos', () => {
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
         cy.get('.woocommerce-message').should('contain', quantidade +' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
             
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 'L', 'Green', 3)
+    });
+
+    it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+        cy.addProdutos('Aero Daily Fitness Tee', 'XL', 'Black', 2)
     });
     
 });
